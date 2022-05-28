17d94bba2e99a9e1eeaab0fb639c95415dfe74d0
ranirizkiani <ranirizkiani1@gmail.com>
2022-05-28 20:58:54 +0700

    modified index.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
-
+const firstUser = {
+    name: "Monica",
+    gender: "Female",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: Array.from(new Set(['Yellow','Pink','White','Purple'])),
+    isHavePet: true,
+    education: [
+        new Map([['name', 'SD 01'], ['city', 'Jakarta'], ['graduate', '2016']]),
+        new Map([['name', 'SMP 02'], ['city', 'Jakarta'], ['graduate', '2019']]),
+        new Map([['name', 'SMA 03'], ['city', 'Tangerang']])
+    ],
+    favoriteRestaurant: Array.from(new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']))
+};
+const secondUser = {
+    name: "Wendy",
+    gender: "Male",
+    age: 23,
+    email: "wendy@dingdong.com",
+    favoriteColor: Array.from(new Set(['Blue','Black','Grey'])),
+    isHavePet: false,
+    education: [
+        new Map([['name', 'SD 02'], ['city', 'Jakarta'], ['graduate', '2010']]),
+        new Map([['name', 'SMP 03'], ['city', 'Bogor'], ['graduate', '2013']]),
+        new Map([['name', 'SMA 01'], ['city', 'Surabaya'], ['graduate', '2016']]),
+        new Map([['name', 'Universitas Maju'], ['city', 'Tangerang']])
+    ],
+    favoriteRestaurant: Array.from(new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Pancake', 'Eggy']))
+};
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
